1d6a29e45db5b4269caa3a0aa47c0ead02928f1d
github username <itsDario1@gmail.com>
2019-06-03 15:40:47 -0400

    setup auth using jwt

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:create]
+        post '/login', to: 'auth#create'
+        get '/profile', to: 'users#profile'
+      end
+    end
+  end
